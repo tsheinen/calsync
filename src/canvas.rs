c336fd91f6a08d2fb3feb626f5e41f4d1e727450
Teddy Heinen <teddy@heinen.dev>
2021-01-30 22:42:25 -0600

    clean up code a bit

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,8 +1,9 @@
 use chrono::{DateTime, Utc};
 use color_eyre::eyre::Result;
+use ics::components::Property;
+use ics::Event;
 use serde::{Deserialize, Serialize};
-#[derive(Debug, Serialize, Deserialize)]
-struct Event {}
+use sha2::{Digest, Sha256};
 
 /// not complete but all i care about lol
 #[derive(Debug, Serialize, Deserialize)]
@@ -19,6 +20,51 @@ pub struct Assignment {
     pub description: Option<String>,
     pub html_url: Option<String>,
     pub due_at: Option<DateTime<Utc>>,
+}
+
+impl Into<Option<Event<'static>>> for Assignment {
+    fn into(self) -> Option<Event<'static>> {
+        let name = self.name.unwrap_or_else(String::new);
+
+        let hash = format!(
+            "{:x}",
+            Sha256::new()
+                .chain(&name.bytes().collect::<Vec<_>>())
+                .chain(
+                    &self
+                        .due_at?
+                        .format("%Y%m%dT%H%M%SZ")
+                        .to_string()
+                        .bytes()
+                        .collect::<Vec<_>>(),
+                )
+                .finalize()
+        );
+
+        Some(
+            vec![
+                Some(Property::new("SUMMARY", name)),
+                Some(Property::new(
+                    "DTSTART",
+                    self.due_at?.format("%Y%m%dT%H%M%SZ").to_string(),
+                )),
+                Some(Property::new(
+                    "DTEND",
+                    self.due_at?.format("%Y%m%dT%H%M%SZ").to_string(),
+                )),
+                self.html_url.map(|url| Property::new("LOCATION", url)),
+            ]
+            .into_iter()
+            .filter_map(|x| x)
+            .fold(
+                Event::new(hash, self.due_at?.format("%Y%m%dT%H%M%SZ").to_string()),
+                |mut acc, prop| {
+                    acc.push(prop);
+                    acc
+                },
+            ),
+        )
+    }
 }
 
 pub async fn get_courses(canvas_url: &str) -> Result<Vec<Course>> {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 mod canvas;
 
-use crate::canvas::get_assignments;
+use crate::canvas::{get_assignments, Assignment};
 use color_eyre::eyre::Result;
-use ics::components::Property;
-use ics::{Event, ICalendar};
-use sha2::{Digest, Sha256};
+use ics::ICalendar;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -23,64 +21,21 @@ async fn main() -> Result<()> {
     dotenv::dotenv()?;
     pretty_env_logger::init();
     let courses = canvas::get_courses(&opts.canvas_url).await?;
-    let assignments = futures::future::join_all(
+    let cal = futures::future::join_all(
         courses
             .iter()
             .map(|x| get_assignments(&opts.canvas_url, x.id)),
     )
     .await
     .into_iter()
-    .filter_map(|x| x.ok())
-    .collect::<Vec<_>>();
-
-    let cal = assignments
-        .into_iter()
-        .flat_map(|x| x.into_iter())
-        .filter_map(|y| {
-            let name = y.name.unwrap_or("".to_string());
-
-            let hash = format!(
-                "{:x}",
-                Sha256::new()
-                    .chain(&name.bytes().collect::<Vec<_>>())
-                    .chain(
-                        &y.due_at?
-                            .format("%Y%m%dT%H%M%SZ")
-                            .to_string()
-                            .bytes()
-                            .collect::<Vec<_>>(),
-                    )
-                    .finalize()
-            );
-
-            Some(
-                vec![
-                    Some(Property::new("SUMMARY", name)),
-                    Some(Property::new(
-                        "DTSTART",
-                        y.due_at?.format("%Y%m%dT%H%M%SZ").to_string(),
-                    )),
-                    Some(Property::new(
-                        "DTEND",
-                        y.due_at?.format("%Y%m%dT%H%M%SZ").to_string(),
-                    )),
-                    y.html_url.map(|url| Property::new("LOCATION", url)),
-                ]
-                .into_iter()
-                .filter_map(|x| x)
-                .fold(
-                    Event::new(hash, y.due_at?.format("%Y%m%dT%H%M%SZ").to_string()),
-                    |mut acc, prop| {
-                        acc.push(prop);
-                        acc
-                    },
-                ),
-            )
-        })
-        .fold(ICalendar::new("2.0", "ics-rs"), |mut acc, ev| {
-            acc.add_event(ev);
-            acc
-        });
+    .filter_map(Result::ok)
+    .into_iter()
+    .flat_map(Vec::into_iter)
+    .filter_map(Assignment::into)
+    .fold(ICalendar::new("2.0", "ics-rs"), |mut acc, ev| {
+        acc.add_event(ev);
+        acc
+    });
 
     if let Some(filename) = opts.output {
         cal.save_file(filename)?;
